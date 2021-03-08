029d850e674c24534d6bc7c3edcc8171a538b47e
False-Logic <80237325+False-Logic@users.noreply.github.com>
2021-03-07 23:44:23 -0500

    Add files via upload

---
+++ Lootbox.py
@@ -0,0 +1,54 @@
+import random as r
+
+for i in range(1000):
+    x = open("Credits.txt", "r")
+    credits = int(x.read())
+    
+    if credits < 100:
+        print("You don't have enough credits!", "\n")
+        print("You can buy more credits in the shop.")
+        break
+        
+    credits -= 100
+    credits = str(credits)
+    x.close()
+    
+    y = open("Credits.txt", "w")
+    edit = y.write(credits)
+    y.close()
+    
+    lootbox_val = r.randrange(1,100) 
+  
+    if lootbox_val in range(1, 65):
+        rarity = "Common"
+        print(rarity)
+    elif lootbox_val in range(65, 80):
+        rarity = "Uncommon"
+        print(rarity)
+    elif lootbox_val in range(80, 91):
+        rarity = "Rare"
+        print(rarity)
+    elif lootbox_val in range(91, 96):
+        rarity = "Epic"
+        print(rarity)
+    elif lootbox_val in range(96, 99):
+        rarity = "Mythic"
+        print(rarity)
+    elif lootbox_val == 99:
+        rarity = "Legendary"
+        print(rarity)
+        
+    z = open("Cards.txt", "a")
+    edit = z.write(rarity + "\n")
+    z.close()
+     
+    
+    cont = input("Play again for 100 credits? (Y/N): ")
+  
+    if cont == "Y":
+        continue
+    elif cont == "N":
+        break
+    else:
+        print("Invalid Input")
+        continue
---
+++ Payment.py
@@ -0,0 +1,59 @@
+for i in range(1000):
+    x = open("Credits.txt", "r")
+    credits = int(x.read())
+    print("\n")
+    print("You have", credits, "credits.", "\n")
+    
+    try:
+        credits_amount = int(input("How many credits would you like to buy?: "))
+    except ValueError:
+        print("\n", "Invalid Input")
+        continue
+    
+    money = format(credits_amount / 100, ".2f")
+    print("\n")
+    print("This will cost $", end = "")
+    print(money, "\n")
+    
+    confirm = input("Confirm your purchase? (Y/N): ")
+    
+    if confirm == "Y":
+        print("\n")
+        print("Confirmed!", "\n")
+        print("A transaction of", "$", end = "")
+        print(money, "has been charged to your account.", "\n")
+        
+    elif confirm == "N":
+        print("\n")
+        print("Transaction Canceled Sucessfully!", "\n")
+        
+        cont = input("Do you still want to purchase credits? (Y/N): ")
+        if cont == "Y":
+            continue
+        elif cont == "N":
+            break
+        else:
+            print("\n")
+            print("Invalid Input")
+            continue
+           
+    credits += credits_amount
+    credits = str(credits)
+    x.close()
+    
+    y = open("Credits.txt", "w")
+    edit = y.write(credits)
+    y.close()
+    
+    print(credits_amount, "credits have been added to your account, bringing your total balance to", credits, "credits", end = "") 
+    print(".", "\n")
+    
+    cont = input("Would you like to buy more credits? (Y/N): ")
+  
+    if cont == "Y":
+        continue
+    elif cont == "N":
+        break
+    else:
+        print("Invalid Input")
+        continue
